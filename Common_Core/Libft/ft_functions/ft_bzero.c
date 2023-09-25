b80f05686bec1c4e8485dad8760293970ed5c848
TiagoAntunes10 <142343478+TiagoAntunes10@users.noreply.github.com>
2023-09-25 15:50:42 +0000

    25/09/2023 - Added ft_bzero and modified ft_memset

--- Common_Core/Libft/ft_functions/ft_bzero.c
+++ Common_Core/Libft/ft_functions/ft_bzero.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void    *ft_memset(void *s, int c, unsigned int n)
+void    ft_bzero(void *s, unsigned int n)
 {
     unsigned int    i;
 
     i = 0;
     while (i < n)
     {
-        *s = c;
+        *s = 0;
         i++;
         s++;
     }
@@ -20,30 +20,28 @@ int main(void)
     char            *str;
     char            *str2;
     unsigned int    n;
-    char            c;
     int             arr[7];
     int             arr2[7];
     int             i;
 
     str = "fhbfdh dhfhbhoasj nhahb";
     str2 = "fhbfdh dhfhbhoasj nhahb";
-    c = '.';
     n = 7;
-    printf("ft_memset_str: %s\n", ft_memset(str + 6, c, sizeof(char) * n));
-    printf("memset_str: %s\n", memset(str2 + 6, c, sizeof(char) * n));
-    arr = ft_memset(arr, c, sizeof(int) * n);
-    arr2 = memset(arr2, c, sizeof(int) * n);
+    printf("ft_bzero_str: %s\n", ft_bzero(str + 6, sizeof(char) * n));
+    printf("bzero_str: %s\n", bzero(str2 + 6, sizeof(char) * n));
+    arr = ft_bzero(arr, sizeof(int) * n);
+    arr2 = bzero(arr2, sizeof(int) * n);
     i = 0;
     while (i < n)
     {
-        printf("ft_memset_arr: %d", arr[i]);
+        printf("ft_bzero_arr: %d", arr[i]);
         i++;
     }
     printf("\n");
     i = 0;
     while (i < n)
     {
-        printf("memset_arr: %d", arr2[i]);
+        printf("bzero_arr: %d", arr2[i]);
         i++;
     }
     printf("\n");
