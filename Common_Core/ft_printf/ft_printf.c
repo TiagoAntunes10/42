7173b2d89addd77e9d5592b217023b007d756b86
TiagoAntunes10 <142343478+TiagoAntunes10@users.noreply.github.com>
2023-10-16 14:50:34 +0000

    16/10/2023 - Started the ft_printf function

--- Common_Core/ft_printf/ft_printf.c
+++ Common_Core/ft_printf/ft_printf.c
@@ -1,7 +1,24 @@
 #include <stdarg.h>
 #include "libft.h"
 
-int	ft_printf(const char *, ...)
+int	ft_printf(const char *str, ... )
 {
-	
+	int		arg;
+	va_list	valst;
+
+	//arg = count_args(str);
+	va_start(valst, arg);
+	while (*str != '\0')
+	{
+		if (*str == '%' && *(str + 1) == '%')
+		{
+			write(1, "%", 1);
+			str++;
+		}
+		else if (*str == '%')
+			//write_spe(str, valst);
+		else
+			write(1, str, 1);
+		str++;
+	}
 }
