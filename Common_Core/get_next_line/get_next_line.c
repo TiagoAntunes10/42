4465202f2451a9ee0e554fc88a3a5116d430a615
TiagoAntunes10 <tfa.antunes@gmail.com>
2023-10-31 23:10:07 +0000

    31/10/2023 - Added get_next_line directory and files. Started the new project.

---
+++ Common_Core/get_next_line/get_next_line.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tialbert <tialbert@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 13:55:10 by tialbert          #+#    #+#             */
+/*   Updated: 2023/10/31 13:55:13 by tialbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	char	*line;
+	int		check;
+
+	line = malloc(100);
+	check = get_line(line, fd);
+	if (check == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
